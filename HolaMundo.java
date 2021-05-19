da3a44f2ca00f4fc383bf6953b1d980cd46b914a
ascastores <83784000+ascastores@users.noreply.github.com>
2021-05-19 13:32:15 -0500

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -8,7 +8,7 @@ public class HolaMundo {
     String naranja = "\033[33m";
     String azul = "\033[34m";
     //Cambio en rama master
-    System.out.println(naranja + "Hola" + azul + " mundo");
+    System.out.println(naranja + "Hola" + azul + " mundo CI");
     //cambio en branch
   }
 }
